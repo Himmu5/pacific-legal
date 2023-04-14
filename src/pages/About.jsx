99bcaa3e73ce22e90ee1cbc1f95b474341b6594a
Jasngh004 <jatin2002sin@gmail.com>
2023-04-14 18:18:08 +0530

    footer and about us done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,12 @@
 import Footer from '../components/footer.js';
+import Hero from "../components/Hero/Hero";
+import AboutBody from '../components/aboutbody.js';
 
 function About(){
     return(
         <>
+        <Hero title="About us"/>
+        <AboutBody/>
         <Footer/>
         </>
     )
