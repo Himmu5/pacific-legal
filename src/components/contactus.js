e8b1a9dc5005a774656e218e211130759b8c88ab
louis <louis.03116412820@ipu.ac.in>
2023-04-17 19:51:09 +0530

    buttonadded

--- src/components/contactus.js
+++ src/components/contactus.js
@@ -1,5 +1,4 @@
 import "./contactus.css";
-
 // import comp9 from "../assets/Vector1.png";
 
 function ContactUsBody() {
@@ -26,6 +25,7 @@ function ContactUsBody() {
             <p>info@pacificlegal.ca</p>
           </div>
         </div>
+        <div className="form-contactus">
         <div className="freeconsult-form">
           <div className="free-consult-left">
             <div className="free-consult-form-fields">
@@ -71,6 +71,12 @@ function ContactUsBody() {
             ></textarea>
           </div>
         </div>
+          <div className='btn-send-message'><button className="btn-message">Send Message</button></div>
+        </div>
+
+
+
+
         <div className="map-location">
         <iframe className='c-map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2887.361676315772!2d-79.42374762264603!3d43.64064298939201!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b3503d5fb9099%3A0x8d720c653c999783!2s5561%20King%20St%20W%20%23688%2C%20Toronto%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sin!4v1681474297722!5m2!1sen!2sin"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
